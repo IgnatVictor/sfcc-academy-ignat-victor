a186dd81c635f3230d1a092b6d167aa7a50f4b2a
kinkey232 <70142353+kinkey232@users.noreply.github.com>
2022-10-27 02:43:12 +0300

    ex4

--- cartridges/app_storefront_custom/cartridge/controllers/Cart.js
+++ cartridges/app_storefront_custom/cartridge/controllers/Cart.js
@@ -1,19 +1,44 @@
-'use strict';
+"use strict";
 
-var server = require('server');
+var server = require("server");
 
 server.extend(module.superModule);
 
-server.append("Show",(req,res,next)=> {
-    var BasketMgr = require('dw/order/BasketMgr');
-    var viewData = res.getViewData();
-    var currentBasket= BasketMgr.getCurrentBasket();
-    var totalPrice= currentBasket.totalGrossPrice;
-    res.viewData.totalPrice= totalPrice.value;
-    res.setViewData(viewData);
+server.append("AddProduct", function(req, res, next) {
+
+    const ProductMgr = require('dw/catalog/ProductMgr');
+    const BasketMgr= require('dw/order/BasketMgr');
+
+    var viewData= res.getViewData();
+    var productId =req.form.pid;
+    var product = ProductMgr.getProduct(productId);
+    var basket= BasketMgr.getCurrentBasket();
+    var image= product.getImage("large").absURL.toString();
+    var email= basket.customer.profile.email;
+   
+
+    importPackage(dw.net);
+    importPackage(dw.value);
+    importPackage(dw.util);
+    importPackage(dw.system);
+
+   
+        var template = new dw.util.Template("mail/mail.isml");
+
+        var o = new dw.util.HashMap();
+        o.put("product", product);
+        o.put("image",image);
+        var text = template.render(o);
+        var mail = new dw.net.Mail();
+        mail.addTo(email);
+        mail.setFrom("noreply@salesforce.com");
+        mail.setSubject("Confirmation of Your Order");
+        mail.setContent(text);
+        if(email) {
+            mail.send();
+        }
 
     next();
-})
-
+});
 
 module.exports = server.exports();
